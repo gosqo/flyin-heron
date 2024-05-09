4eb846df0430352953ef79c1b5492c12e2b7c4f4
KimHyeonBong <vongright@gmail.com>
2024-05-09 18:57:07 +0900

    fix: 추가 Board DTO 생성자
    빌더로 직접 생성해 사용하고 있었는데, 테스트 중 스프링이 해당 DTO 에 매핑하지 못하는 상황이 발생하여 추가.

--- src/main/java/com/vong/manidues/board/dto/BoardDeleteResponse.java
+++ src/main/java/com/vong/manidues/board/dto/BoardDeleteResponse.java
@@ -1,10 +1,14 @@
 package com.vong.manidues.board.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class BoardDeleteResponse {
     private boolean isDeleted;
     private String message;
--- src/main/java/com/vong/manidues/board/dto/BoardUpdateResponse.java
+++ src/main/java/com/vong/manidues/board/dto/BoardUpdateResponse.java
@@ -1,10 +1,14 @@
 package com.vong.manidues.board.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class BoardUpdateResponse {
     private Long id;
     private boolean isUpdated;
