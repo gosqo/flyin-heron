e7a7f6bc6326b71902a82b218dd30dbbafe9c5b7
KimHyeonBong <vongright@gmail.com>
2024-05-29 10:18:22 +0900

    remove: 사용하지 않는 메서드, 클래스 삭제

--- src/main/java/com/vong/manidues/token/TokenRepository.java
+++ src/main/java/com/vong/manidues/token/TokenRepository.java
@@ -1,33 +1,14 @@
 package com.vong.manidues.token;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface TokenRepository extends JpaRepository<Token, Long> {
-    @Query("""
-            SELECT t
-              FROM Token t
-             INNER JOIN Member m
-                   ON t.member.id = m.id
-             WHERE m.id = :memberId
-            """)
-    List<Token> findAllValidTokensByMember(@Param("memberId") Long memberId);
-
     Optional<Token> findByToken(String token);
-
     List<Token> findAllByToken(String token);
-
     @Transactional
     int deleteByToken(String token);
-
-    /*
-    * deleteByToken 과 차이를 알아보기 위한 테스트용 메서드
-    * */
-    @Transactional
-    int deleteAllByToken(String token);
 }
