e177169749bb7fd6b83d53a7b75cf6aa0c92fd2e
KimHyeonBong <vongright@gmail.com>
2024-08-12 19:25:11 +0900

    update: EnableJpaAuditing 애너테이션 config 클래스로 분리
    * WebMvcTest 시 jpaAuditingHandler 빈을 생성하지 못하는 문제로 해당 애너테이션 config 클래스로 분리.

--- src/main/java/com/vong/manidues/ManiDues.java
+++ src/main/java/com/vong/manidues/ManiDues.java
@@ -2,10 +2,8 @@ package com.vong.manidues;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
 public class ManiDues {
     public static void main(String[] args) {
         SpringApplication.run(ManiDues.class, args);
