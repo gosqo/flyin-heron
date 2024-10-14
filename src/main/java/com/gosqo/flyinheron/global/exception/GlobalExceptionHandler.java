fb66c08b1b46c3872bfd64c89d3981fd336d26e2
KimHyeonBong <vongright@gmail.com>
2024-10-14 19:14:28 +0900

    refactor: GlobalExceptionHandler 리팩토링, 요청 메서드에 따른 예외 처리

--- src/main/java/com/gosqo/flyinheron/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/gosqo/flyinheron/global/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.gosqo.flyinheron.global.exception;
 
 import com.gosqo.flyinheron.global.exception.custom.DebugNeededException;
 import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
@@ -20,47 +21,151 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
 
-import java.util.Arrays;
 import java.util.NoSuchElementException;
 import java.util.Objects;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @ControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
-    private static final String PROJECT_ROOT_PACKAGE = "com.gosqo.flyinheron";
+    private static final Set<Class<? extends Exception>> BAD_REQUEST_EXCEPTIONS = Set.of(
+            MethodArgumentNotValidException.class
+            , MethodArgumentTypeMismatchException.class
+            , BadCredentialsException.class
+            , DataIntegrityViolationException.class
+            , IllegalArgumentException.class
+    );
 
-    private static StackTraceElement[] filterProjectStackTrace(StackTraceElement[] stackTrace) {
-        return Arrays.stream(stackTrace)
-                .filter(element -> element.getClassName().startsWith(PROJECT_ROOT_PACKAGE))
-                .toArray(StackTraceElement[]::new);
+    private static final Set<Class<? extends Exception>> UNAUTHORIZED_EXCEPTIONS = Set.of(
+            ExpiredJwtException.class
+    );
+
+    private static final Set<Class<? extends Exception>> FORBIDDEN_EXCEPTIONS = Set.of(
+            javax.naming.AuthenticationException.class
+            , AccessDeniedException.class
+            , JwtException.class
+    );
+    private static final Set<Class<? extends Exception>> NOT_FOUND_EXCEPTIONS = Set.of(
+            NoResourceFoundException.class
+            , NoSuchElementException.class
+            , HttpRequestMethodNotSupportedException.class
+    );
+    private static final Set<Class<? extends Exception>> INTERNAL_SERVER_EXCEPTIONS = Set.of(
+            NullPointerException.class
+            , DebugNeededException.class
+            , RuntimeException.class
+            , Exception.class
+    );
+
+    private static final Set<Class<? extends Exception>> STACK_TRACE_NEEDED = Set.of(
+            NullPointerException.class
+            , DebugNeededException.class
+            , RuntimeException.class
+            , Exception.class
+    );
+
+    @ExceptionHandler(Exception.class)
+    public Object handleGetRequestExceptions(Exception e, HttpServletRequest request, HttpServletResponse response) {
+
+        if (request.getMethod().equalsIgnoreCase(HttpMethod.GET.name())) {
+
+            if (BAD_REQUEST_EXCEPTIONS.contains(e.getClass())) {
+                return "error/400";
+            }
+
+            if (UNAUTHORIZED_EXCEPTIONS.contains(e.getClass())) {
+                return "error/401";
+            }
+
+            if (FORBIDDEN_EXCEPTIONS.contains(e.getClass())) {
+                return "error/403";
+            }
+
+            if (NOT_FOUND_EXCEPTIONS.contains(e.getClass())) {
+                return "error/403";
+            }
+
+            if (INTERNAL_SERVER_EXCEPTIONS.contains(e.getClass())) {
+                return "error/500";
+            }
+        }
+
+        return delegateHandling(response, e);
     }
 
-    private static StackTraceElement getFirstProjectFileFromStackTrace(StackTraceElement[] stackTrace) {
-        return filterProjectStackTrace(stackTrace)[0];
-    }
+    public <E extends Exception> ResponseEntity<ErrorResponse> delegateHandling(
+            HttpServletResponse response,
+            E e
+    ) {
+        log.warn(exceptionNameAndMessage(e));
 
-    private static StackTraceElement getFirstFileFromStackTrace(Exception ex) {
-        return ex.getStackTrace()[0];
-    }
+        // log stack trace if needed.
+        if (STACK_TRACE_NEEDED.contains(e.getClass())) {
+            log.warn("", e);
+        }
 
-    private static String whichFileThrow(Exception ex) {
-        return getFirstFileFromStackTrace(ex).getFileName();
-    }
+        // exception instance check.
+        if (e instanceof ExpiredJwtException) {
+            return buildResponseEntity(HttpStatus.UNAUTHORIZED, "회원이라면 잠시 후 다시 시도해주세요.");
+        }
 
-    private static String whichMethodThrow(Exception ex) {
-        return getFirstFileFromStackTrace(ex).getMethodName();
+        if (e instanceof BadCredentialsException) {
+            return buildResponseEntity(HttpStatus.BAD_REQUEST, "이메일 혹은 패스워드를 확인 해주세요.");
+        }
+
+        if (e instanceof AccessDeniedException) {
+            return buildResponseEntity(HttpStatus.FORBIDDEN, "로그인 후 이용해주시기 바랍니다.");
+        }
+
+        if (e instanceof AuthenticationException) {
+            return buildResponseEntity(HttpStatus.FORBIDDEN, "인증 정보에 문제가 있습니다.");
+        }
+
+        if (e instanceof MethodArgumentNotValidException) {
+            return buildResponseEntity(
+                    HttpStatus.BAD_REQUEST
+                    , ((MethodArgumentNotValidException) e).getFieldErrors().get(0).getDefaultMessage()
+            );
+        }
+
+        if (e instanceof DataIntegrityViolationException) {
+            return buildResponseEntity(HttpStatus.CONFLICT, "자원의 입력 및 수정이 중복을 발생시킵니다. 이 중복은 허용되지 않습니다.");
+        }
+
+        if (e instanceof NoSuchElementException
+                || e instanceof NoResourceFoundException
+        ) {
+            return buildResponseEntity(HttpStatus.NOT_FOUND, "존재하지 않는 자원에 대한 요청입니다.");
+        }
+
+        if (e instanceof IllegalArgumentException
+                || e instanceof MethodArgumentTypeMismatchException
+        ) {
+            return buildResponseEntity(HttpStatus.BAD_REQUEST, "올바르지 않은 요청입니다.");
+        }
+
+        if (e instanceof HttpRequestMethodNotSupportedException) {
+            String supportedMethod = Objects.requireNonNull(
+                            ((HttpRequestMethodNotSupportedException) e).getSupportedHttpMethods())
+                    .stream()
+                    .map(HttpMethod::toString)
+                    .collect(Collectors.joining(", "));
+
+            response.setHeader(HttpHeaders.ALLOW, supportedMethod);
+
+            return buildResponseEntity(HttpStatus.METHOD_NOT_ALLOWED, "API 사용에 합의되지 않은 요청입니다.");
+        }
+
+        if (e instanceof RuntimeException) {
+            return buildResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR, "서버 오류가 발생했습니다. 문제가 지속될 시 운영진에 연락 부탁드립니다.");
+        }
+
+        return buildResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR, "서버 오류가 발생했습니다.");
     }
 
     private String exceptionNameAndMessage(Exception ex) {
         return String.format("%s: %s", ex.getClass().getName(), ex.getMessage());
-    }
-
-    private String whichThrow(Exception ex) {
-        return String.format("thrown in: %s\n\tclosest Project files is: %s"
-                , getFirstFileFromStackTrace(ex)
-                , getFirstProjectFileFromStackTrace(ex.getStackTrace())
-        );
     }
 
     private ResponseEntity<ErrorResponse> buildResponseEntity(
@@ -72,206 +177,5 @@ public class GlobalExceptionHandler {
                         .status(status.value())
                         .message(message)
                         .build());
-    }
-
-    @ExceptionHandler(AccessDeniedException.class)
-    public Object handleAccessDeniedException(
-            AccessDeniedException ex
-            , HttpServletRequest request
-            , HttpServletResponse response
-    ) {
-        log.info(exceptionNameAndMessage(ex));
-        log.warn("", ex);
-
-        if (request.getMethod().equalsIgnoreCase(HttpMethod.GET.name())) {
-            response.setStatus(HttpStatus.FORBIDDEN.value());
-            return "error/403";
-        }
-
-        String userMessage = "로그인 후 이용해주시기 바랍니다.";
-
-        return buildResponseEntity(HttpStatus.FORBIDDEN, userMessage);
-    }
-
-    @ExceptionHandler(NoResourceFoundException.class)
-    public Object handleNoResourceFoundException(
-            NoResourceFoundException ex
-            , HttpServletResponse response
-    ) {
-        log.info(exceptionNameAndMessage(ex));
-        log.warn(whichThrow(ex));
-
-        if (ex.getHttpMethod().matches(HttpMethod.GET.name())) {
-            response.setStatus(404);
-            return "error/404";
-        }
-
-        String userMessage = "존재하지 않는 자원에 대한 요청입니다.";
-
-        return buildResponseEntity(HttpStatus.NOT_FOUND, userMessage);
-    }
-
-    @ExceptionHandler(BadCredentialsException.class)
-    public ResponseEntity<ErrorResponse> handleBadCredentialsException(
-            BadCredentialsException ex
-    ) {
-        log.info(exceptionNameAndMessage(ex));
-        log.warn(whichThrow(ex));
-        String userMessage = "이메일 혹은 패스워드를 확인 해주세요.";
-
-        return buildResponseEntity(HttpStatus.BAD_REQUEST, userMessage);
-    }
-
-    @ExceptionHandler(DataIntegrityViolationException.class)
-    public ResponseEntity<?> handleDataIntegrityViolationException(
-            DataIntegrityViolationException ex
-    ) {
-        log.info(exceptionNameAndMessage(ex));
-        log.warn(whichThrow(ex));
-
-        String userMessage;
-
-        switch (whichMethodThrow(ex)) {
-            case "isUniqueEmail" -> {
-                userMessage = "이미 가입한 이메일입니다.";
-                return buildResponseEntity(HttpStatus.CONFLICT, userMessage);
-            }
-            case "isUniqueNickname" -> {
-                userMessage = "사용 중인 닉네임입니다.";
-                return buildResponseEntity(HttpStatus.CONFLICT, userMessage);
-            }
-            default -> userMessage = "자원의 입력 및 수정이 지정된 형식에 맞지 않거나 중복을 발생시킵니다.";
-        }
-
-        log.warn("", ex);
-
-        return buildResponseEntity(HttpStatus.BAD_REQUEST, userMessage);
-    }
-
-    @ExceptionHandler(AuthenticationException.class)
-    public ResponseEntity<?> handleAuthenticationException(
-            AuthenticationException ex
-    ) {
-        log.warn("", ex);
-        String userMessage = "인증에 실패했습니다.";
-
-        return buildResponseEntity(HttpStatus.BAD_REQUEST, userMessage);
-    }
-
-    @ExceptionHandler(NoSuchElementException.class)
-    public ResponseEntity<ErrorResponse> handleNoSuchElementException(
-            NoSuchElementException ex
-    ) {
-        log.info(exceptionNameAndMessage(ex));
-        log.warn(whichThrow(ex));
-        String userMessage = "존재하지 않는 자원에 대한 요청입니다.";
-
-        return buildResponseEntity(HttpStatus.BAD_REQUEST, userMessage);
-    }
-
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(
-            MethodArgumentNotValidException ex
-    ) {
-        log.info(exceptionNameAndMessage(ex));
-        log.warn(whichThrow(ex));
-        String userMessage = ex.getFieldErrors().get(0).getDefaultMessage();
-
-        return buildResponseEntity(HttpStatus.BAD_REQUEST, userMessage);
-    }
-
-    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
-    public ResponseEntity<ErrorResponse> handleMethodArgumentTypeMismatchException(
-            MethodArgumentTypeMismatchException ex
-    ) {
-        log.info(exceptionNameAndMessage(ex));
-        log.warn(whichThrow(ex));
-        String userMessage = "올바른 요청이 아닙니다.";
-
-        return buildResponseEntity(HttpStatus.BAD_REQUEST, userMessage);
-    }
-
-    @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<ErrorResponse> handleIllegalArgumentException(
-            IllegalArgumentException ex
-    ) {
-        log.warn(ex.getMessage(), ex);
-        String userMessage = "올바른 요청이 아닙니다.";
-
-        return buildResponseEntity(HttpStatus.BAD_REQUEST, userMessage);
-    }
-
-    @ExceptionHandler(NullPointerException.class)
-    public ResponseEntity<ErrorResponse> handleNullPointerException(
-            NullPointerException ex
-    ) {
-        log.warn(ex.getMessage(), ex);
-        String userMessage = "서버 오류가 발생했습니다. 문제가 지속될 시 운영진에 연락 부탁드립니다.";
-
-        return buildResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR, userMessage);
-    }
-
-    @ExceptionHandler(ExpiredJwtException.class)
-    public ResponseEntity<ErrorResponse> handleExpiredJwtException(
-            ExpiredJwtException ex
-    ) {
-        log.info(exceptionNameAndMessage(ex));
-        String userMessage = "토큰이 만료되어 서버에 도달했습니다.";
-
-        return buildResponseEntity(HttpStatus.UNAUTHORIZED, userMessage);
-    }
-
-    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
-    public Object handleHttpRequestMethodNotSupportedException(
-            HttpServletRequest request
-            , HttpServletResponse response
-            , HttpRequestMethodNotSupportedException ex
-    ) {
-        log.info(exceptionNameAndMessage(ex));
-
-        if (request.getMethod().matches(HttpMethod.GET.name())) {
-            response.setStatus(404);
-            return "error/404";
-        }
-
-        String userMessage = "API 사용에 합의되지 않은 요청입니다.";
-        String supportedMethod = Objects.requireNonNull(ex.getSupportedHttpMethods())
-                .stream()
-                .map(HttpMethod::toString)
-                .collect(Collectors.joining(", "));
-
-        response.setHeader(HttpHeaders.ALLOW, supportedMethod);
-
-        return buildResponseEntity(HttpStatus.METHOD_NOT_ALLOWED, userMessage);
-    }
-
-    @ExceptionHandler(DebugNeededException.class)
-    public ResponseEntity<ErrorResponse> handleDebugNeededException(
-            DebugNeededException ex
-    ) {
-        log.warn("", ex);
-        String userMessage = "서버 오류가 발생했습니다. 문제가 지속될 시 운영진에 연락 부탁드립니다.";
-
-        return buildResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR, userMessage);
-    }
-
-    @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<ErrorResponse> handleRuntimeException(
-            RuntimeException ex
-    ) {
-        log.warn("", ex);
-        String userMessage = "서버 오류가 발생했습니다. 문제가 지속될 시 운영진에 연락 부탁드립니다.";
-
-        return buildResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR, userMessage);
-    }
-
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorResponse> handleException(
-            Exception ex
-    ) {
-        log.warn("", ex);
-        String userMessage = "서버 오류가 발생했습니다. 문제가 지속될 시 운영진에 연락 부탁드립니다.";
-
-        return buildResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR, userMessage);
     }
 }
